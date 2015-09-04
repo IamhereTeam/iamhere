5f13634bf0298079af3021c853bdca3675b8fd21
Grig <yeghiazaryan.grigor@iamcloud.com>
2015-09-04 14:29:54 +0400

    IamHere Started

--- iamhere/iamhere/MainActivity.cs
+++ iamhere/iamhere/MainActivity.cs
@@ -5,13 +5,21 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Locations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace iamhere
 {
     [Activity(Label = "iamhere", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : Activity, ILocationListener
     {
-        int count = 1;
+        Location _currentLocation;
+        LocationManager _locationManager;
+        TextView _locationText;
+        TextView _addressText;
+        String _locationProvider;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -20,11 +28,94 @@ namespace iamhere
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            _addressText = FindViewById<TextView>(Resource.Id.address_text);
+            _locationText = FindViewById<TextView>(Resource.Id.location_text);
+            FindViewById<TextView>(Resource.Id.get_address_button).Click += AddressButton_OnClick;
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            InitializeLocationManager();
+        }
+        void InitializeLocationManager()
+        {
+            _locationManager = (LocationManager)GetSystemService(LocationService);
+            Criteria criteriaForLocationService = new Criteria
+            {
+                Accuracy = Accuracy.Fine
+            };
+            IList<string> acceptableLocationProviders = _locationManager.GetProviders(criteriaForLocationService, true);
+
+            if (acceptableLocationProviders.Any())
+            {
+                _locationProvider = acceptableLocationProviders.First();
+            }
+            else
+            {
+                _locationProvider = String.Empty;
+            }
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            _locationManager.RequestLocationUpdates(_locationProvider, 0, 0, this);
+        }
+        protected override void OnPause()
+        {
+            base.OnPause();
+            _locationManager.RemoveUpdates(this);
+        }
+        async void AddressButton_OnClick(object sender, EventArgs eventArgs)
+        {
+            if (_currentLocation == null)
+            {
+                _addressText.Text = "Can't determine the current address.";
+                return;
+            }
+
+            Geocoder geocoder = new Geocoder(this);
+            IList<Address> addressList = await geocoder.GetFromLocationAsync(_currentLocation.Latitude, _currentLocation.Longitude, 10);
+
+            Address address = addressList.FirstOrDefault();
+            if (address != null)
+            {
+                StringBuilder deviceAddress = new StringBuilder();
+                for (int i = 0; i < address.MaxAddressLineIndex; i++)
+                {
+                    deviceAddress.Append(address.GetAddressLine(i))
+                                 .AppendLine(",");
+                }
+                _addressText.Text = deviceAddress.ToString();
+            }
+            else
+            {
+                _addressText.Text = "Unable to determine the address.";
+            }
+        }
+
+        public void OnLocationChanged(Location location)
+        {
+            _currentLocation = location;
+            if (_currentLocation == null)
+            {
+                _locationText.Text = "Unable to determine your location.";
+            }
+            else
+            {
+                _locationText.Text = String.Format("{0},{1}", _currentLocation.Latitude, _currentLocation.Longitude);
+            }
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
+        {
+            throw new NotImplementedException();
         }
     }
 }
